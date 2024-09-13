349cb47bb7e43a9e0bc3d9595a728ee5f4821112
albertahnlide <albert.ahnlide@outlook.com>
2024-09-13 13:39:27 +0200

    Lab0 klar

---
+++ labs/lab0/include/lab0.js
@@ -0,0 +1,5 @@
+// const elem = document.getElementById("my-div");
+// elem.innerHTML = "<h1>Hello DOM</h1>";
+
+// const formInput = '<img src="x" onerror="alert(\'your page has been hacked\');">';
+// elem.innerHTML = formInput;
