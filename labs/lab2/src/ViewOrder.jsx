14b1f8fa88468c32ecb70df563ca348a5250b1fe
albertahnlide <albert.ahnlide@outlook.com>
2024-09-25 08:50:44 +0200

    Update ViewOrder.jsx

--- labs/lab2/src/ViewOrder.jsx
+++ labs/lab2/src/ViewOrder.jsx
@@ -4,13 +4,14 @@ function ViewOrder({cart}) {
     return (
         <div className="row h-200 p-5 bg-light border rounded-3">
           <h2>Varukorgen</h2>
-          <ul>
+          <fieldset>
+            
           {cart.map((salad) => (
             // <li key={index}>Salad {index + 1}: {salad.getIngredients() + ", " + salad.getPrice()+"kr" }</li> 
-            <li key={salad.uuid}>{salad.getIngredients() + ", pris: " + salad.getPrice()+"kr" }</li> 
+            <div key={salad.uuid}>{salad.getIngredients() + ", pris: " + salad.getPrice()+"kr" }</div> 
             
           ))}
-        </ul>
+        </fieldset>
         </div>          
     );
     }
