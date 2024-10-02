19f28a81095aaa764d403aa17f65007420209ea9
albertahnlide <albert.ahnlide@outlook.com>
2024-10-02 13:16:24 +0200

    Lab3

--- labs/lab4/src/ErrorBoundary.jsx
+++ labs/lab4/src/ErrorBoundary.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { useOutletContext } from 'react-router-dom';
+import Navbar from './Navbar';
 
 function ErrorBoundary(props) {
 
-    return (
-        <div className="row h-200 p-5 bg-light border rounded-3">
-          <h2>404 Page not found</h2>
-        </div>          
-    );
+  return (
+    <div className="container py-4">
+    <header className="pb-3 mb-4 border-bottom">
+        <span className="fs-4">Min egen salladsbar</span>
+      </header>
+
+      <Navbar />
+
+      <div className=" p-4 border rounded-3 mb-2" style={{ backgroundColor: 'rgba(200, 0, 0, 0.9)' }}>
+        <h1>Page not found</h1>
+      </div>
+
+      
+    <footer className="pt-3 mt-4 text-muted border-top">
+        EDAF90 - webprogrammering
+      </footer>
+    </div>
+  );
     }
 
 export default ErrorBoundary;
