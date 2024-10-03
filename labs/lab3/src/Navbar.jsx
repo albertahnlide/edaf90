6191585370472818e3ea6d9be22cb9807a7871f8
albertahnlide <albert.ahnlide@outlook.com>
2024-10-03 14:31:57 +0200

    Update Navbar.jsx

--- labs/lab3/src/Navbar.jsx
+++ labs/lab3/src/Navbar.jsx
@@ -8,12 +8,15 @@ export default function Navbar() {
                 Startsida
             </NavLink>
             </li>
+            <li className="nav-item">
             <NavLink className="nav-link" to="/compose-salad">
                 Komponera en sallad
             </NavLink>
-
+            </li>
+            <li className="nav-item">
             <NavLink className="nav-link" to="/view-order">
                 Varukorgen
             </NavLink>
+            </li>
         </ul>);
 }
