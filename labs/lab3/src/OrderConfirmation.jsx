367ec8e77d3a22b890d775c39d8bbfe1717eff04
albertahnlide <albert.ahnlide@outlook.com>
2024-10-03 14:54:05 +0200

    Update OrderConfirmation.jsx

--- labs/lab3/src/OrderConfirmation.jsx
+++ labs/lab3/src/OrderConfirmation.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import { useOutletContext } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom'; 
-import { useEffect } from 'react';
-
 
 const OrderConfirmation = () => {
   const { cart = [] } = useOutletContext(); 
   const { uuid } = useParams(); 
   const item = cart.find((item) => item.uuid === uuid); 
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if(!item) {
-      navigate("/view-order");
-    }
-  }, [item, navigate]);
-
   return (
-    item ? (
       <div className="alert alert-success">
-        <div key="confirmation">
+        <div key={item.uuid}>
           En sallad har lagts till i varukorgen. {item.getIngredients()}, pris: {item.getPrice()}kr
         </div>
       </div>
-    ) : null
   );
 };
 
 export default OrderConfirmation;
-
-
-
